c322806b0b53803b9833bce9fa08826d12bce512
mariav1993 <126624816+mvaldez1993@users.noreply.github.com>
2023-03-15 23:17:16 +0000

    done

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 
 
 
-
-
-
 import random  
 
 n1 = random.randint(0,100)
@@ -13,7 +10,7 @@ print (n1, n2, n3)
 
 if n1 < n2 and n1 <n3:
   print (f'{n1} is the smallest number')
-if n2 < n1 and n2 < n3:
+elif n2 < n1 and n2 < n3:
     print (f'{n2} is the smallest number')
 else:
-      print ('f{n3} is the smallest number')
+      print (f'{n3} is the smallest number')
